fa193e6444a93eefa1a80f26a0eea3b307ec49fc
rnlabez <119831235+rnlabez@users.noreply.github.com>
2023-03-07 06:10:46 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'rnlabez_news';
 
     public function getNews($slug = false)
     {
